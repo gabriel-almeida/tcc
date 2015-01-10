ec48f3e5c8441783ef57edcbc6681cf320587931
gabriel-almeida <gabriel.almeida.cp2@gmail.com>
2015-01-10 01:05:50 -0200

    wrapper de algumas metricas de similaridade de string da biblioteca second string

--- TCC/src/utilidades/MetricasSimilaridade.java
+++ TCC/src/utilidades/MetricasSimilaridade.java
@@ -1,5 +1,25 @@
 package utilidades;
 
-public class MetricasSimilaridade {
+import com.wcohen.ss.JaroWinkler;
+import com.wcohen.ss.MongeElkan;
+import com.wcohen.ss.ScaledLevenstein;
 
+public class MetricasSimilaridade {
+	/**
+	 * Wrapper de algumas metricas de similadidade.
+	 * */
+	
+	public static MongeElkan me = new MongeElkan();
+	public static JaroWinkler jw = new JaroWinkler();
+	public static ScaledLevenstein l = new ScaledLevenstein();
+	
+	public static double MongeElkan(String a, String b){
+		return me.score(a, b);
+	}
+	public static double JaroWinkler(String a, String b){
+		return jw.score(a, b);
+	}
+	public static double Levenstein(String a, String b){
+		return l.score(a, b);
+	}
 }
