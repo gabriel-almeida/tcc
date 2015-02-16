0a349210d254be703eac1c158b35689ea70c4408
gabriel-almeida <gabriel.almeida.cp2@gmail.com>
2015-02-16 01:49:26 -0200

    Melhorado Entrada CSV, agora usado Stream API do Java 8

--- TCC/src/entrada_saida/EntradaCSV.java
+++ TCC/src/entrada_saida/EntradaCSV.java
@@ -3,15 +3,19 @@ package entrada_saida;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import modelo.Elemento;
 import utilidades.AnalisePerformace;
 
 public class EntradaCSV {
+	private static int tamMaxChave = 9;
+
 	private String delimitador = "\";\"";
 	private List<String> colunasRelevantes;
 	private List<String> tipoColunas;
@@ -19,7 +23,8 @@ public class EntradaCSV {
 
 	private String colunaChave;
 	private int indiceChave;
-	
+	private Map<String, Integer> colunasMapeadas;
+
 	public EntradaCSV(String arqCsv, List<String> colunasRelevantes, List<String> tipoColunas, String colunaChave) {
 		this.colunasRelevantes = colunasRelevantes;
 		this.colunaChave = colunaChave;
@@ -53,63 +58,56 @@ public class EntradaCSV {
 	}
 	/**
 	 * Implementacao concorrente da leitura de CSV. 
+	 * Caso exista uma repeticao de chaves, os elementos afetados serao impressos e apenas um deles sera usado aleatoriamente
 	 * O mapa retornado eh thread-safe
 	 * */
+	@SuppressWarnings("resource")
 	public Map<String, Elemento> leCsv() throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(arqCsv));
 
-		Map<String, Elemento> resultado = new ConcurrentHashMap<String, Elemento>();
-		List<Integer> colunasValidas = new ArrayList<Integer>();
-		
-		indiceChave = -1;
-
-		//inicializo essa lista com indices -1
-		for (int i=0; i< colunasRelevantes.size(); i++){
-			colunasValidas.add(-1);
-		}
+		this.colunasMapeadas = new HashMap<String, Integer>();
 		
 		//LOG tempo
 		AnalisePerformace tempo = new AnalisePerformace();
-
-		//Localizo os cabecalhos relevantes e boto seus indices na ordem da lista de nomes
-		String cabecalho[] = br.readLine().split(delimitador);
-		for (int i = 0; i< cabecalho.length; i++){
-			String nomeColunaAtual = cabecalho[i].replaceAll("\"", "");
-
-			if (nomeColunaAtual.equals(colunaChave)){ //caso chave
-				indiceChave = i;
-				continue;
-			}
-
-			int indiceColuna = colunasRelevantes.indexOf(nomeColunaAtual);
-			if (indiceColuna != -1){
-				colunasValidas.set(indiceColuna, i);
-			}
-		}
-
-		br.lines().parallel().forEach( (String linha) -> {
-			String campos[] = linha.split(delimitador);//TODO pensar em algo melhor
-			
-			//TODO pensar em algo melhor do que essa remocao de aspas
-			String chave = campos[indiceChave].replaceAll("\"",""); 
-			chave = chave.substring(0, 9); //TODO substring gambiarra, pensar numa solucao melhor
-
-			Elemento e = new Elemento(chave, colunasRelevantes, tipoColunas);
-			int contador = 0;
-			for (int i: colunasValidas){
-				e.addElemento(contador, campos[i].replaceAll("\"", ""));
-				contador++;
-			}
-			resultado.put(chave, e);
-
-		});
 		
+		this.parseiaCabecalho(br.readLine());
+		
+		Map<String, Elemento> resultado = br.lines().parallel()
+				.map(this::parseiaLinha)
+				.collect(Collectors.toConcurrentMap(Elemento::getChave, Function.identity(), 
+						(chaveA, chaveB) -> {
+							System.out.println("Repetido: " + chaveA + " -> " + chaveB); //Essa funcao eh chamada quando ha uma colisao de chaves
+							return chaveB;
+						} ));
+
 		//Log Tempo
 		tempo.capturaTempo(resultado.size());
 		tempo.imprimeEstatistica("Lendo " + arqCsv);
-		
+
 		br.close();
 		return resultado;
+	}
+
+	private Elemento parseiaLinha(String linha){
+		String campos[] = linha.split(delimitador);
+
+		String chave = campos[indiceChave].replaceAll("\"","").substring(0, tamMaxChave); //TODO rever esse substring 
+		Elemento e = new Elemento(chave, this.colunasRelevantes, this.tipoColunas);
+		
+		this.colunasRelevantes.stream()
+			.forEach(colunaAtual -> e.addElemento(colunaAtual, campos[this.colunasMapeadas.get(colunaAtual)].replaceAll("\"", "")) );
+		
+		return e;
+	}
+	private void parseiaCabecalho(String s){
+		List<String> cabecalho = Arrays.stream(s.split(delimitador))
+				.map(campo -> campo.replaceAll("\"", ""))
+				.collect(Collectors.toList());
+		
+		this.indiceChave = cabecalho.indexOf(this.colunaChave);
+		
+		this.colunasMapeadas = this.colunasRelevantes.stream()
+			.collect(Collectors.toMap(Function.identity(), coluna -> cabecalho.indexOf(coluna)));
 	}
 
 }
